09ba9df50d9b23c82b92515e17642d3fb216b8b2
David R <david2005.rosental@gmail.com>
2026-03-24 02:02:39 +0000

    Added validation function for non-negative fields

--- validation/common.go
+++ validation/common.go
@@ -14,3 +14,23 @@ func ValidateRequiredFields(v interface{}) error {
 	}
 	return nil
 }
+
+// ValidateNonNegativeFields checks if fields marked with validate:"nonnegative" are non-negative numbers.
+func ValidateNonNegativeFields(v interface{}) error {
+	nonNegativeFields := ValidateByTag(v, "nonnegative", func(field reflect.StructField, value reflect.Value) bool {
+		switch value.Kind() {
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+			return value.Int() < 0
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			return false // unsigned types are always non-negative
+		case reflect.Float32, reflect.Float64:
+			return value.Float() < 0
+		default:
+			return false
+		}
+	})
+	if len(nonNegativeFields) > 0 {
+		return &ValidationError{Fields: nonNegativeFields}
+	}
+	return nil
+}
